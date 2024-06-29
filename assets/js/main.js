dd19a508b0b4fa4439deb03d73a922d07e229db9
zobirofkir <zobirofkir19@gmail.com>
2024-06-29 08:39:01 +0100

    Create Public Folder

--- assets/js/main.js
+++
@@ -1,6 +0,0 @@
-const button = document.querySelector('button[data-collapse-toggle="navbar-default"]');
-const nav = document.getElementById('navbar-default');
-
-button.addEventListener('click', () => {
-    nav.classList.toggle('hidden');
-});
